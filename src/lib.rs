16fa43233da60540309aa47a7b9542be07321627
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-02-02 15:03:50 +0800

    use "fatal:" for prefix for error messages

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,53 @@
 use std::{fmt::Display, process::exit};
 
-/// See [`stop`].
-pub fn stop_impl(msg: impl Display) -> ! {
+/// See [`fatal`].
+pub fn fatal_hook(msg: impl Display) -> ! {
     #[cfg(feature = "colored")]
     use colored::Colorize;
     #[cfg(feature = "colored")]
-    let prefix = "error".bold().red();
+    let prefix = "fatal:".bold().red();
     #[cfg(not(feature = "colored"))]
-    let prefix = "error";
-    eprintln!("{prefix}: {msg}");
+    let prefix = "fatal:";
+    eprintln!("{prefix} {msg}");
     exit(-1)
 }
 
 /// Print an error message and stop the program.
-/// 
-/// This would use [`Display`] to format the message and add an "error:" prefix.
-/// 
-/// Exit code is always `-1`.
+///
+/// This would use [`Display`] to format the message and add a "fatal:" prefix.
+/// If the `colored` feature is enabled, the prefix would be colored in red and bold using ANSI escape codes.
+///
+/// Process is immediately terminated with exit code -1.
 ///
 /// # Examples
 ///
 /// ```rust
-/// use stop::stop;
+/// use stop::fatal;
 ///
 /// fn display() -> ! {
-///     stop!(42) // error: 42
+///     fatal!(42) // fatal: 42
 /// }
 ///
 /// fn format() -> ! {
-///     stop!("this is {}", "an error") // error: this is an error
+///     fatal!("this is {}", "an error") // fatal: this is an error
 /// }
 ///
 /// fn result(result: Result<(), Box<dyn std::error::Error>>) {
-///     result.unwrap_or_else(stop!()); // `stop!()` returns a function
+///     result.unwrap_or_else(fatal!()); // `fatal!()` returns a function
 /// }
 ///
 /// ```
 #[macro_export]
-macro_rules! stop {
+macro_rules! fatal {
     () => {
-        |x| stop!(x)
+        |x| fatal!(x)
     };
     ($err:expr) => {
         {
             #[allow(unreachable_code)]
             {
                 let err = $err;
-                $crate::stop_impl(format!("{err}"));
+                $crate::fatal_hook(format!("{err}"));
             };
         }
     };
@@ -54,7 +55,7 @@ macro_rules! stop {
         {
             #[allow(unreachable_code)]
             {
-                $crate::stop_impl(format_args!($($arg)*));
+                $crate::fatal_hook(format_args!($($arg)*));
             };
         }
     };
